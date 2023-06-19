84656c5fb331c2510cad47bd331bcf73afa68622
JuanFranciscoGrillo <134559638+JuanFranciscoGrillo@users.noreply.github.com>
2023-06-19 18:50:03 -0500

    Review 06-19-23

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# Challenge_03-Javascript
-Repository contains Module 3 Challenge
+1. Project's Title
+
+Password Generator
+
+2. Project's Description
+
+This project is a password generator that allows users to generate secure passwords based on their desired criteria, such as length and character types.
+
+3. Table of Contents:
+
+	a. How to install and run the project
+
+	b. How to use the project
+
+4. How to install and run the project:
+
+	a. To install and run the password generator project, follow these steps:
+
+		I. Click on GitHub repository link:
+				
+		https://github.com/JuanFranciscoGrillo/Challenge_03-Javascript.git
+			
+		II. Click on deployed website link:
+				
+		https://juanfranciscogrillo.github.io/Challenge_03-Javascript/
+
+	b. Running the project locally
+
+		I. Open the project folder.
+		
+		II. Double-click on the index.html file to open it in a web browser.
+
+		III. The password generator will be displayed in the browser.
+
+5. How to use the project:
+
+	a. To use the password generator, follow these steps:
+
+		I. Open the password generator in a web browser by following the installation instructions.
+		
+		II. Choose a password length between 8 and 128 characters when prompted.
+		
+		III. Confirm or deny whether the password should contain lowercase characters, uppercase characters, numeric characters, and special characters.
+		
+		IV. Once you have made your choices, click on the "Generate Password" button.
+		
+		V. The generated password will be displayed in the text area below the button.
+		
+		VI. You can copy the generated password and use it as desired.
+		
+6. Screenshot:
+	
+	a. assets\screenshot\Password Generator.gif
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,82 @@
-// Assignment code here
-
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
 function writePassword() {
-    var password = generatePassword();
+    // Ask the user for the desired password length
+    var length = prompt("Choose a password length between 8 and 128 characters:");
+    console.log("Selected length:", length);
+
+    // Ask if the password should contain lowercase letters
+    var containsLowercase = confirm("Contains lowercase characters?");
+    console.log("Contains lowercase:", containsLowercase);
+
+    // Ask if the password should contain uppercase letters
+    var containsUppercase = confirm("Contains uppercase characters?");
+    console.log("Contains uppercase:", containsUppercase);
+
+    // Ask if the password should contain numbers
+    var containsNumeric = confirm("Contains numeric characters?");
+    console.log("Contains numeric:", containsNumeric);
+
+    // Ask if the password should contain special characters
+    var containsSpecial = confirm("Contains special characters?");
+    console.log("Contains special:", containsSpecial);
+
+    // Check if the password length is within the valid range
+    if (length < 8 || length > 128) {
+        alert("Invalid password length. Must be between 8 and 128 characters.");
+        console.log("Invalid password length.");
+        return;
+    }
+
+    // Check if at least one character type is selected
+    if (!(containsLowercase || containsUppercase || containsNumeric || containsSpecial)) {
+        alert("Please select at least one character type for the password.");
+        console.log("No character type selected.");
+        return;
+    }
+
+    // Generate the password based on user choices
+    var password = generatePassword(length, containsLowercase, containsUppercase, containsNumeric, containsSpecial);
     var passwordText = document.querySelector("#password");
 
+    console.log("Generated Password:", password);
+
+    // Display the generated password
     passwordText.value = password;
 
+    alert("Password generated!");
 }
 
-// Add event listener to generate button
+function generatePassword(length, containsLowercase, containsUppercase, containsNumeric, containsSpecial) {
+    var characterSet = "";
+    var lowercaseSet = "abcdefghijklmnopqrstuvwxyz";
+    var uppercaseSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var numericSet = "0123456789";
+    var specialSet = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
+
+    // Build the character set based on user choices
+    if (containsLowercase) {
+        characterSet += lowercaseSet;
+    }
+    if (containsUppercase) {
+        characterSet += uppercaseSet;
+    }
+    if (containsNumeric) {
+        characterSet += numericSet;
+    }
+    if (containsSpecial) {
+        characterSet += specialSet;
+    }
+
+    console.log("Character set:", characterSet);
+
+    var password = "";
+    // Generate the password by randomly selecting characters from the character set
+    for (var i = 0; i < length; i++) {
+        password += characterSet.charAt(Math.floor(Math.random() * characterSet.length));
+    }
+
+    return password;
+}
+
 generateBtn.addEventListener("click", writePassword);
